d9c84e86c13538aa1786b30cfb848a5e53433927
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2023-02-13 12:16:46 +0000

    c

--- main.py
+++
@@ -1,7 +0,0 @@
-numero_magico = 12345679
-numero_usuario = int(input("Introduce un número entre 1 y 9: "))
-
-numero_usuario *= 9
-numero_magico *= numero_usuario
-
-print("El número mágico es: ", numero_magico)
